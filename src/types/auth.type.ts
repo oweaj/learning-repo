8e8cfb54a599f968cb68622f1a0141dbe1baccd1
oweaj <qwe1520156@naver.com>
2025-04-14 04:32:08 +0900

    chore: data type 설정

---
+++ src/types/auth.type.ts
@@ -0,0 +1,13 @@
+export interface LoginDataType {
+  email: string;
+  password: string;
+}
+
+export interface userDataType {
+  email: string;
+  id: string | null;
+  name: string | null;
+  phone_number: string | null;
+  profile_image: string | null;
+  status: string;
+}
---
+++ src/types/blog.type.ts
@@ -0,0 +1,47 @@
+import type { userDataType } from "./auth.type";
+
+export interface BlogPageParamsType {
+  page?: string;
+  [key: string]: string | string[] | undefined;
+}
+
+export interface BlogListType {
+  id: number | null;
+  title: string;
+  content: string | null;
+  main_image: string;
+  sub_image: string;
+  created_at: string;
+  deleted_at: string | null;
+  updated_at: string;
+  category: {
+    id: number | null;
+    name: string;
+  };
+  user: userDataType;
+}
+
+export type PartialBlogDataType = Partial<BlogListType>;
+
+export interface BlogDetailDataType {
+  user: {
+    id: number;
+    email: "string";
+    status: "string";
+    name: "string";
+    phone_number: "string";
+    profile_image: "string";
+  };
+  category: {
+    id: number;
+    name: "string";
+  };
+  id: number;
+  created_at: "string";
+  updated_at: "string";
+  deleted_at: "string";
+  title: "string";
+  main_image: "string";
+  sub_image: "string";
+  content: "string";
+}
