ec508e52ec1cfee26baa7852597ffeead08cce5a
oweaj <qwe1520156@naver.com>
2025-04-13 22:57:10 +0900

    chore: svg 폴더 추가 및 svgr webpack 설정

--- next.config.ts
+++ next.config.ts
@@ -2,26 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule: { test?: RegExp }) =>
-      rule.test?.test?.(".svg"),
-    );
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: { not: /.*/ }, // *.svg
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: /component/, // *.svg?component
-        use: ["@svgr/webpack"],
-      },
-    );
-
-    fileLoaderRule.exclude = /\.svg$/i;
-
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
     return config;
   },
 };
