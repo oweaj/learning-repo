47c78e8122a3ab5ed7c9479f02730236e3fdc1ec
oweaj <qwe1520156@naver.com>
2025-04-14 22:01:12 +0900

    chore: 상수 데이터

---
+++ src/constants/blogCategory.ts
@@ -0,0 +1,7 @@
+export const BLOG_CATEGORY = [
+  { value: 0, label: "전체" },
+  { value: 1, label: "일상생활" },
+  { value: 2, label: "맛집소개" },
+  { value: 3, label: "제품후기" },
+  { value: 4, label: "IT정보" },
+];
