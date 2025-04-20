137b82f6cf1bdd3b1cf992d9dcda556b023ef5a2
oweaj <qwe1520156@naver.com>
2025-04-21 00:47:53 +0900

    fix: 블로그 생성 form 유효성 설정

--- src/schemas/blog.schema.ts
+++ src/schemas/blog.schema.ts
@@ -1,10 +1,13 @@
 import * as z from "zod";
 
 export const BlogCreateSchema = z.object({
-  title: z.string(),
-  main_image: z.string(),
+  title: z
+    .string()
+    .min(1, "제목은 필수입니다.")
+    .max(20, "제목은 최대 20자입니다"),
+  main_image: z.string().min(1, "대표 이미지는 필수입니다"),
   sub_image: z.string(),
-  category: z.number(),
-  content: z.string(),
+  category: z.number().min(1, "카테고리를 선택해주세요"),
+  content: z.string().min(10, "내용을 입력해주세요"),
 });
 export type BlogFormValues = z.infer<typeof BlogCreateSchema>;
