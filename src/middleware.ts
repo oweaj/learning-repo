a830f4113c509905364dc7f42cf9a8e02162502a
oweaj <qwe1520156@naver.com>
2025-04-23 10:21:19 +0900

    chore: 미들웨어 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,16 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get("access");
   const loginPage = request.nextUrl.pathname === "/login";
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  if (loginPage) {
+    if (token) {
+      return NextResponse.redirect(new URL("/0", request.url));
+    }
+
+    return NextResponse.next();
   }
 
-  if (loginPage) {
-    return NextResponse.redirect(new URL("/0", request.url));
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
