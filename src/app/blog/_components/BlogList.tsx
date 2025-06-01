d252f085df4977d56ca0163e53346b0def73db2d
oweaj <qwe1520156@naver.com>
2025-06-01 16:41:28 +0900

    fix: 페이지네이션 그룹 페이지 설정 및 이전, 다음 버튼 추가 수정

--- src/app/blog/_components/BlogList.tsx
+++ src/app/blog/_components/BlogList.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useBlogList } from "@/lib/queries/blog/useBlogList";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useRouter } from "next/navigation";
 import BlogCard from "./BlogCard";
 
@@ -13,13 +14,28 @@ const BlogList = ({
   if (!data || !count) return null;
 
   const totalPage = Math.ceil(count / 10);
-  const pageNumber = Array.from({ length: totalPage }, (_, i) => i + 1);
+  const groupPage = 5;
+  const currentGroup = Math.floor((page - 1) / groupPage);
+  const startPage = currentGroup * groupPage + 1;
+  const endPage = Math.min(startPage + groupPage - 1, totalPage);
+  const pageNumberList = Array.from(
+    { length: endPage - startPage + 1 },
+    (_, i) => startPage + i,
+  );
 
   const handlePageChange = (pageNumber: number) => {
     const query = category
       ? `/?category=${category}&page=${pageNumber}&limit=10`
       : `/?page=${pageNumber}&limit=10`;
     router.push(query);
+  };
+
+  const handleMovePage = (move: "prev" | "next") => {
+    if (move === "prev" && page > 1) {
+      handlePageChange(page - 1);
+    } else if (move === "next" && page < totalPage) {
+      handlePageChange(page + 1);
+    }
   };
 
   return (
@@ -34,18 +50,41 @@ const BlogList = ({
           <BlogCard key={item.id} {...item} />
         ))}
       </ul>
+
       <ul className="flex items-center justify-center gap-4 mt-16">
-        {pageNumber.map((num) => (
+        <li>
+          <button
+            type="button"
+            className={`flex items-center ${page !== 1 && "cursor-pointer"}`}
+            onClick={() => handleMovePage("prev")}
+            disabled={page === 1}
+          >
+            <ChevronLeft className="w-7 h-7" />
+          </button>
+        </li>
+        {pageNumberList.map((num) => (
           <li key={num}>
             <button
               type="button"
               onClick={() => handlePageChange(num)}
-              className={`px-4 py-2 rounded ${num === page ? "bg-orange-500 text-white" : "bg-gray-200"}`}
+              className={`px-4 py-2 rounded cursor-pointer ${
+                num === page ? "bg-orange-500 text-white" : "bg-gray-200"
+              }`}
             >
               {num}
             </button>
           </li>
         ))}
+        <li>
+          <button
+            type="button"
+            className={`flex items-center ${page !== totalPage && "cursor-pointer"}`}
+            onClick={() => handleMovePage("next")}
+            disabled={page === totalPage}
+          >
+            <ChevronRight className="w-7 h-7" />
+          </button>
+        </li>
       </ul>
     </div>
   );
