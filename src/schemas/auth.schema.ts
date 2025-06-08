f7ce21f3198e41970e3bfa84b2f81cce0ee6575b
oweaj <qwe1520156@naver.com>
2025-06-08 22:50:22 +0900

    fix: 인증 form zod 유효성 수정

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -2,7 +2,7 @@ import * as z from "zod";
 
 export const signinSchema = z.object({
   email: z.string().email("이메일 형식으로 입력해주세요."),
-  password: z.string().min(6, "비밀번호는 최소 1자 이상입니다."),
+  password: z.string().min(6, "비밀번호는 최소 6자 이상입니다."),
 });
 
 export const signupSchema = z.object({
@@ -10,7 +10,7 @@ export const signupSchema = z.object({
   email: z.string().email("이메일 형식으로 입력해주세요."),
   password: z
     .string()
-    .min(1, "비밀번호는 최소 6자 이상입니다.")
+    .min(6, "비밀번호는 최소 6자 이상입니다.")
     .regex(
       /^(?=.*[a-z])(?=.*\d)[a-z\d]+$/i,
       "비밀번호는 영문자와 숫자를 포함해야 합니다.",
