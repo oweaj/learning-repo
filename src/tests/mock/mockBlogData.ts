1380a584e2dc3133045894e7f0ce66b324597a34
oweaj <qwe1520156@naver.com>
2025-06-09 21:53:41 +0900

    test: 테스트 공통 mock data 생성

---
+++ src/tests/mock/mockBlogData.ts
@@ -0,0 +1,20 @@
+import type { TBlogListType } from "@/types/blog.type";
+
+export const mockBlogData: TBlogListType = {
+  id: 1,
+  user_id: {
+    id: "userId",
+    email: "test@test.com",
+    name: "테스트",
+    profile_image: null,
+    status: null,
+  },
+  title: "테스트 제목",
+  main_image: "/test-image.jpg",
+  sub_image: null,
+  category_id: { id: 1, name: "daily_life" },
+  content: "테스트 내용",
+  created_at: "2025-01-01T00:00:00.000Z",
+  deleted_at: null,
+  updated_at: null,
+};
