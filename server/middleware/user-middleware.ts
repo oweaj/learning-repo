20c8b322b972ca34406e815fc8a405b265470a8e
oweaj <qwe1520156@naver.com>
2025-06-23 23:51:44 +0900

    feat: 로그인 유저 정보

---
+++ server/middleware/user-middleware.ts
@@ -0,0 +1,29 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import type { IUserRequest } from "../utils/jwt.js";
+
+export interface IAuthRequest extends Request {
+  user: IUserRequest;
+}
+
+// 현재 로그인한 유저 정보
+export const isLoginUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const token = req.cookies.accessToken;
+
+  if (!token) {
+    res.status(400).json({ message: "토큰이 존재하지 않습니다." });
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    (req as IAuthRequest).user = decoded as IUserRequest;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: `유효하지 않은 토큰입니다. : ${error}` });
+  }
+};
