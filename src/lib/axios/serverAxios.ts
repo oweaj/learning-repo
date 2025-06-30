8172e1b0486ddac88ed5037b47f509fcfc192cc5
oweaj <qwe1520156@naver.com>
2025-06-30 11:22:20 +0900

    chore: 서버 인스턴스 헤더 쿠키 수정

--- src/lib/axios/serverAxios.ts
+++ src/lib/axios/serverAxios.ts
@@ -3,22 +3,15 @@
 import axios from "axios";
 import { cookies } from "next/headers";
 
-export const serverAxios = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  withCredentials: true,
-});
+export const serverAxios = async () => {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("accessToken")?.value;
 
-serverAxios.interceptors.request.use(async (config) => {
-  try {
-    const cookieStore = await cookies();
-    const token = cookieStore.get("authToken")?.value;
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  } catch (error) {
-    console.log(`서버 토큰 헤더 설정 에러 : ${error}`);
-  }
-
-  return config;
-});
+  return axios.create({
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+    withCredentials: true,
+    headers: {
+      Cookie: token,
+    },
+  });
+};
