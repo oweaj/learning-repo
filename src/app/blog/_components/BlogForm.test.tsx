978ff3229831f84f2e9e4a0a4d57a462d80162d9
oweaj <qwe1520156@naver.com>
2025-07-01 14:40:48 +0900

    test: blogform 테스트 코드 수정

--- src/app/blog/_components/BlogForm.test.tsx
+++ src/app/blog/_components/BlogForm.test.tsx
@@ -41,17 +41,11 @@ describe("blogform 컴포넌트", () => {
   it("블로그 form 항목들이 렌더링 되어야한다.", () => {
     render(<BlogForm editMode={false} />);
 
-    expect(screen.getByRole("textbox", { name: /타이틀/ })).toHaveAttribute(
-      "name",
-      "title",
-    );
+    expect(screen.getByRole("textbox", { name: /타이틀/ }));
     expect(screen.getByTestId("main-image-upload")).toBeInTheDocument();
     expect(screen.getByTestId("sub-image-upload")).toBeInTheDocument();
     expect(screen.getByRole("combobox")).toBeInTheDocument();
-    expect(screen.getByRole("textbox", { name: /내용/ })).toHaveAttribute(
-      "name",
-      "content",
-    );
+    expect(screen.getByRole("textbox", { name: /내용/ }));
   });
 
   it("수정폼(editmode)일경우 기존 데이터가 렌더링되어야한다.", () => {
@@ -59,7 +53,7 @@ describe("blogform 컴포넌트", () => {
       <BlogForm
         editMode
         defaultData={mockBlogData}
-        id={`${mockBlogData.id}`}
+        id={`${mockBlogData._id}`}
       />,
     );
 
@@ -70,9 +64,7 @@ describe("blogform 컴포넌트", () => {
     expect(
       within(screen.getByRole("combobox")).getByText("일상생활"),
     ).toBeInTheDocument();
-    expect(
-      screen.getByText(mockBlogData.content as string),
-    ).toBeInTheDocument();
+    expect(screen.getByText(mockBlogData.content)).toBeInTheDocument();
   });
 
   it("form에 데이터가 입력이 되어있을 경우에 네비게이션 가드가 활성화되면 comfirm 메세지 창이 표시된다.", () => {
@@ -80,7 +72,7 @@ describe("blogform 컴포넌트", () => {
       <BlogForm
         editMode
         defaultData={mockBlogData}
-        id={`${mockBlogData.id}`}
+        id={`${mockBlogData._id}`}
       />,
     );
 
@@ -115,11 +107,11 @@ describe("blogform 컴포넌트", () => {
     });
 
     // 이미지 업로드
-    const file = new File(["test-image"], "test-image.jpg", {
+    const file = new File(["test-image"], mockBlogData.main_image, {
       type: "image/jpeg",
     });
 
-    mockImageUpload.mockResolvedValue("/test-image.jpg");
+    mockImageUpload.mockResolvedValue({ url: mockBlogData.main_image });
 
     fireEvent.click(screen.getByTestId("main-image-upload"));
     fireEvent.change(screen.getByTestId("main-image-upload"), {
@@ -145,9 +137,9 @@ describe("blogform 컴포넌트", () => {
       expect(mockBlogCreate).toHaveBeenCalledTimes(1);
       expect(mockBlogCreate).toHaveBeenCalledWith({
         title: mockBlogData.title,
-        main_image: "/test-image.jpg",
+        main_image: mockBlogData.main_image,
         sub_image: null,
-        category_id: mockBlogData.category_id.id,
+        category_id: mockBlogData.category_id,
         content: mockBlogData.content,
       });
     });
