f8c59c16e5b8a9a84fd7ebbbe4a150795a69dfe1
oweaj <qwe1520156@naver.com>
2025-07-05 13:54:57 +0900

    feat: 로그인 유저 헤더 name 표시

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,13 +1,23 @@
+"use client";
+
 import { Logout } from "@/app/auth/_components/Logout";
+import { useUser } from "@/lib/queries/blog/useUser";
 import Link from "next/link";
 
 const Header = () => {
+  const user = useUser();
+
   return (
     <header className="max-w-screen-xl flex items-center justify-between py-6 mx-auto px-4">
       <Link href="/">
         <h1 className="text-3xl font-black text-orange-400">BLOG</h1>
       </Link>
-      <Logout />
+      {user && (
+        <div className="flex items-center justify-between gap-4">
+          <span className="font-semibold">{user.name}님</span>
+          <Logout />
+        </div>
+      )}
     </header>
   );
 };
