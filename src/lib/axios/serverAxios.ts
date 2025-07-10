31a91111ada721442888b609d78f877d3d8e9083
oweaj <qwe1520156@naver.com>
2025-07-10 22:16:24 +0900

    fix: 인스턴스 수정

--- src/lib/axios/serverAxios.ts
+++ src/lib/axios/serverAxios.ts
@@ -1,17 +1,6 @@
-"use server";
-
 import axios from "axios";
-import { cookies } from "next/headers";
 
-export const serverAxios = async () => {
-  const cookieStore = await cookies();
-  const token = cookieStore.get("accessToken")?.value;
-
-  return axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-    withCredentials: true,
-    headers: {
-      Cookie: token,
-    },
-  });
-};
+export const serverAxios = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  withCredentials: true,
+});
