52617fb558a0586f0bdfb5cb7b817472fe8087c3
oweaj <qwe1520156@naver.com>
2025-07-14 16:33:21 +0900

    fix: 상세 type 수정

--- src/lib/queries/blog/useBlogDetail.ts
+++ src/lib/queries/blog/useBlogDetail.ts
@@ -1,9 +1,9 @@
 import { blogDetailApi } from "@/lib/api/blog/blog";
-import type { IBlogListType } from "@/types/blog.type";
+import type { IBlogDataType } from "@/types/blog.type";
 import { useQuery } from "@tanstack/react-query";
 
 export const useBlogDetail = ({ id }: { id: string }) => {
-  const data = useQuery<IBlogListType>({
+  const data = useQuery<IBlogDataType>({
     queryKey: ["blogDetail", id],
     queryFn: () => blogDetailApi(id),
   });
