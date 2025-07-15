6cec9504e5a753a353f9d5a366d8444724376bf0
oweaj <qwe1520156@naver.com>
2025-07-15 22:57:59 +0900

    fix: 헤더 로그아웃 이동 및 테스트코드 수정

---
+++ src/components/home/Header.test.tsx
@@ -0,0 +1,81 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Header from "@/components/home/Header";
+
+const mockLogout = jest.fn();
+const mockAlert = jest.fn();
+const mockRouterPush = jest.fn();
+window.alert = mockAlert;
+
+jest.mock("@/lib/queries/auth/useLogout", () => ({
+  useLogout: () => ({
+    mutate: mockLogout,
+  }),
+}));
+
+jest.mock("next/navigation", () => ({
+  useRouter: () => ({
+    push: mockRouterPush,
+  }),
+}));
+
+describe("헤더 컴포넌트", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("로고, 마이페이지, 로그아웃 요소가 렌더링된다.", () => {
+    render(<Header />);
+
+    expect(
+      screen.getByRole("heading", { level: 1, name: "BLOG" }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "마이페이지" }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "로그아웃" }),
+    ).toBeInTheDocument();
+  });
+
+  it("로그아웃 실패시 에러 메시지가 표시된다.", () => {
+    mockLogout.mockImplementation(() => {
+      const errorLogout = new Error("로그아웃 실패 메세지");
+      alert(errorLogout.message);
+    });
+
+    render(<Header />);
+
+    expect(
+      screen.getByRole("button", { name: "로그아웃" }),
+    ).toBeInTheDocument();
+
+    fireEvent.click(screen.getByRole("button", { name: "로그아웃" }));
+    expect(mockLogout).toHaveBeenCalledTimes(1);
+    expect(mockAlert).toHaveBeenCalledWith("로그아웃 실패 메세지");
+  });
+
+  it("로그아웃 버튼 클릭시 로그아웃 쿼리 실행이된다.", () => {
+    render(<Header />);
+
+    expect(
+      screen.getByRole("button", { name: "로그아웃" }),
+    ).toBeInTheDocument();
+
+    fireEvent.click(screen.getByRole("button", { name: "로그아웃" }));
+    expect(mockLogout).toHaveBeenCalledTimes(1);
+
+    screen.debug();
+  });
+
+  it("마이페이지 링크 클릭시 마이페이지로 이동된다.", () => {
+    render(<Header />);
+
+    expect(
+      screen.getByRole("button", { name: "마이페이지" }),
+    ).toBeInTheDocument();
+
+    fireEvent.click(screen.getByRole("button", { name: "마이페이지" }));
+    expect(mockRouterPush).toHaveBeenCalledWith("/mypage");
+  });
+});
--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,23 +1,34 @@
 "use client";
 
-import { Logout } from "@/app/auth/_components/Logout";
-import { useUser } from "@/lib/queries/blog/useUser";
+import { useLogout } from "@/lib/queries/auth/useLogout";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
-  const user = useUser();
+  const { mutate: logout } = useLogout();
+  const router = useRouter();
 
   return (
     <header className="max-w-screen-xl flex items-center justify-between py-6 mx-auto px-4">
       <Link href="/">
         <h1 className="text-3xl font-black text-orange-400">BLOG</h1>
       </Link>
-      {user && (
-        <div className="flex items-center justify-between gap-4">
-          <span className="font-semibold">{user.name}님</span>
-          <Logout />
-        </div>
-      )}
+      <div className="flex items-center justify-between gap-4 text-sm">
+        <button
+          type="button"
+          className="cursor-pointer hover:font-semibold"
+          onClick={() => router.push("/mypage")}
+        >
+          마이페이지
+        </button>
+        <button
+          type="button"
+          className="cursor-pointer hover:font-semibold"
+          onClick={() => logout()}
+        >
+          로그아웃
+        </button>
+      </div>
     </header>
   );
 };
