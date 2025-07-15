ef4f05928cc2b055213a9d716409ef4ac785a7b0
oweaj <qwe1520156@naver.com>
2025-07-15 22:55:01 +0900

    chore: 하단 네비 chat 아이콘 제거

--- src/constants/bottomNavbar.ts
+++ src/constants/bottomNavbar.ts
@@ -1,11 +1,9 @@
-import Chat from "@/assets/icons/page_chat.svg";
 import Home from "@/assets/icons/page_home.svg";
 import Rank from "@/assets/icons/page_rank.svg";
 import User from "@/assets/icons/page_user.svg";
 
 export const BOTTOM_NAV_LIST = [
   { id: "home", link: "/", Icon: Home },
-  { id: "chat", link: "/", Icon: Chat },
-  { id: "rank", link: "/", Icon: Rank },
-  { id: "user", link: "/", Icon: User },
+  { id: "rank", link: "/blog_rank", Icon: Rank },
+  { id: "user", link: "/mypage", Icon: User },
 ];
---
+++ src/constants/mypageSidebar.ts
@@ -0,0 +1,16 @@
+export const MYPAGE_SIDE_BAR = [
+  {
+    groupName: "블로그 정보",
+    items: [
+      { name: "등록한 블로그", path: "/" },
+      { name: "추천한 블로그", path: "/" },
+    ],
+  },
+  {
+    groupName: "내 정보",
+    items: [
+      { name: "프로필 수정", path: "/" },
+      { name: "회원 탈퇴", path: "/" },
+    ],
+  },
+];
