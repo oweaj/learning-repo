9dd4484aa3a683f9c23f865920be3d8f2ee993b1
oweaj <qwe1520156@naver.com>
2025-07-26 17:10:15 +0900

    feat: 마이페이지 서버 라우트 변경 및 추가

--- server/routes/mypage-routes.ts
+++ server/routes/mypage-routes.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { blogLike, myBlogs } from "../controllers/blog.js";
+import { blogLike } from "../controllers/blog.js";
+import { myBlogs, noticeCreate, noticeList } from "../controllers/mypage.js";
 import { isLoginUser } from "../middleware/user-middleware.js";
 
 const router = express.Router();
 
+router.post("/notice/create", isLoginUser, noticeCreate);
+router.get("/notice/list", isLoginUser, noticeList);
 router.get("/blog", isLoginUser, myBlogs);
 router.patch("/blog-like", isLoginUser, blogLike);
 
