7c08be075bdbfc0204a95f27eb386aba97b84a9f
oweaj <qwe1520156@naver.com>
2025-07-27 23:39:42 +0900

    chore: 유저 정보 데이터 수정

--- src/constants/mypage/mypageUserInfo.ts
+++ src/constants/mypage/mypageUserInfo.ts
@@ -1,13 +1,16 @@
 import { NotebookPen } from "lucide-react";
-import { MousePointerClick } from "lucide-react";
 import { Star } from "lucide-react";
 import { Pencil } from "lucide-react";
 import { Megaphone } from "lucide-react";
 
 export const MYPAGE_USER_INFO = [
-  { name: "내 블로그 수", path: "/my/blogs", Icon: NotebookPen },
-  { name: "최대 조회 수", path: null, Icon: MousePointerClick },
-  { name: "최대 공감 수", path: null, Icon: Star },
-  { name: "최대 댓글 수", path: null, Icon: Pencil },
-  { name: "공지사항", path: "/my/notice", Icon: Megaphone },
+  {
+    id: "blogCount",
+    name: "내 블로그 수",
+    path: "/my/blogs",
+    Icon: NotebookPen,
+  },
+  { id: "likeCount", name: "최대 공감 수", path: null, Icon: Star },
+  { id: "reviewCount", name: "최대 댓글 수", path: null, Icon: Pencil },
+  { id: "notice", name: "공지사항", path: "/my/notice", Icon: Megaphone },
 ];
