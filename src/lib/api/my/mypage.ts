82ea2e42868d1f0011eb1336a3150d30eaca726d
oweaj <qwe1520156@naver.com>
2025-07-31 15:41:45 +0900

    feat: 마이페이지 공지 api 및 query 생성

--- src/lib/api/my/mypage.ts
+++ src/lib/api/my/mypage.ts
@@ -28,6 +28,48 @@ export const noticeListApi = async () => {
   }
 };
 
+// 공지사항 수정
+export const noticeUpdateApi = async ({
+  id,
+  formData,
+}: { id: string; formData: INoticeFormDataType }) => {
+  try {
+    const { data } = await clientAxios.patch(`/api/my/notice/${id}`, formData);
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const message = error.response?.data?.message || error.message;
+      throw new Error(message);
+    }
+  }
+};
+
+// 공지사항 상세
+export const noticeDetailApi = async (id: string) => {
+  try {
+    const { data } = await clientAxios.get(`/api/my/notice/${id}`);
+    return data.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const message = error.response?.data?.message || error.message;
+      throw new Error(message);
+    }
+  }
+};
+
+// 공지사항 삭제
+export const noticeDeleteApi = async (id: string) => {
+  try {
+    const { data } = await clientAxios.delete(`/api/my/notice/${id}`);
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const message = error.response?.data?.message || error.message;
+      throw new Error(message);
+    }
+  }
+};
+
 // 등록한 블로그
 export const myblogListApi = async () => {
   try {
--- src/lib/queries/my/useNoticeList.ts
+++ src/lib/queries/my/useNoticeList.ts
@@ -1,5 +1,3 @@
-noticeListApi;
-
 import { noticeListApi } from "@/lib/api/my/mypage";
 import type { INoticeDataType } from "@/types/mypage.type";
 import { useQuery } from "@tanstack/react-query";
