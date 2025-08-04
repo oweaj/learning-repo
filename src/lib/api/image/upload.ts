5b3ff3b475b0a1c6ec430e39608ba35e1a6b0f7c
oweaj <qwe1520156@naver.com>
2025-08-04 19:43:21 +0900

    fix: 이미지 변경 시 이전 이미지 삭제 후 업로드

--- src/lib/api/image/upload.ts
+++ src/lib/api/image/upload.ts
@@ -1,12 +1,23 @@
 import { clientAxios } from "@/lib/axios/clientAxios";
 
-export const imageUploadApi = async (file: File) => {
+export const imageUploadApi = async (
+  prefix: string,
+  file: File,
+  prevImage?: string,
+) => {
+  if (!file) throw new Error("이미지 파일이 없습니다.");
+
   const formData = new FormData();
   formData.append("file", file);
 
-  if (!file) throw new Error("이미지 파일이 없습니다.");
+  if (prevImage) {
+    formData.append("prevImage", prevImage);
+  }
 
-  const { data } = await clientAxios.post("/api/image/upload", formData);
+  const { data } = await clientAxios.post(
+    `/api/image/upload/${prefix}`,
+    formData,
+  );
 
   return data;
 };
