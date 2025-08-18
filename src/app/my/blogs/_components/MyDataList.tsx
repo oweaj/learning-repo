5252fb16c2cf84abfb09ae32bd19446415bd8707
oweaj <qwe1520156@naver.com>
2025-08-18 23:42:33 +0900

    fix: 마이페이지 블로드 데이터 공통 조회 컴포넌트 수정

--- src/app/my/blogs/_components/MyDataList.tsx
+++ src/app/my/blogs/_components/MyDataList.tsx
@@ -1,30 +1,35 @@
 "use client";
 
 import BlogCard from "@/app/blog/_components/BlogCard";
-import { useMyBlogList } from "@/lib/queries/my/useMyBlog";
 import type { IBlogDataType } from "@/types/blog.type";
 import { BlogSkeleton } from "./BlogSkeleton";
 
-const MyBlogList = ({ preview = false }: { preview?: boolean }) => {
-  const { data, isLoading, error } = useMyBlogList();
+interface IMyBlogsType {
+  preview?: boolean;
+  data?: IBlogDataType[];
+  isLoading: boolean;
+}
 
-  const renderData = preview ? data?.blogs.slice(0, 3) : data?.blogs;
+const MyDataList = ({ preview, data, isLoading }: IMyBlogsType) => {
+  const renderData = preview ? data?.slice(0, 3) : data;
 
   return (
     <div>
       {isLoading ? (
         <BlogSkeleton limit={preview ? 3 : 5} />
-      ) : data && data.blogs.length > 0 ? (
+      ) : data && data.length > 0 ? (
         <ul className="space-y-8">
           {renderData?.map((item: IBlogDataType) => (
             <BlogCard key={item._id} {...item} />
           ))}
         </ul>
       ) : (
-        <div className="text-center text-gray-500 py-10">{error?.message}</div>
+        <div className="text-center text-gray-500 py-10">
+          작성한 블로그가 없습니다
+        </div>
       )}
     </div>
   );
 };
 
-export default MyBlogList;
+export default MyDataList;
