d51f88be735f8342f643b0eb234f092b6c49cfac
oweaj <qwe1520156@naver.com>
2025-08-18 10:51:48 +0900

    fix: 공통 하단바 현재 위치 스타일 수정

--- src/components/home/BottomNavbar.tsx
+++ src/components/home/BottomNavbar.tsx
@@ -2,6 +2,7 @@
 
 import { BOTTOM_NAV_LIST } from "@/constants/bottomNavbar";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import type { ComponentType, SVGProps } from "react";
 
 export interface INavItemType {
@@ -12,13 +13,17 @@ export interface INavItemType {
 }
 
 const BottomNavbar = () => {
+  const pathname = usePathname();
+
   return (
     <nav className="w-full fixed bottom-0 left-0 right-0 h-14 shadow-[0_-3px_5px_rgba(0,0,0,0.1)] bg-white">
       <ul className="h-full flex gap-10 items-center justify-around">
         {BOTTOM_NAV_LIST.map(({ name, path, Icon }: INavItemType) => (
           <li key={name} className="p-1">
             <Link href={path ? path : "/"}>
-              <Icon className="w-6 h-6" />
+              <Icon
+                className={`w-7 h-7 stroke-[1.5] ${pathname === path && "stroke-orange-500"}`}
+              />
             </Link>
           </li>
         ))}
--- src/constants/bottomNavbar.ts
+++ src/constants/bottomNavbar.ts
@@ -1,9 +1,9 @@
-import Home from "@/assets/icons/page_home.svg";
-import Rank from "@/assets/icons/page_rank.svg";
-import User from "@/assets/icons/page_user.svg";
+import { House } from "lucide-react";
+import { Trophy } from "lucide-react";
+import { UserRound } from "lucide-react";
 
 export const BOTTOM_NAV_LIST = [
-  { name: "home", path: "/", Icon: Home },
-  { name: "rank", path: "/blog_rank", Icon: Rank },
-  { name: "user", path: "/my", Icon: User },
+  { name: "home", path: "/", Icon: House },
+  { name: "rank", path: "/blog_rank", Icon: Trophy },
+  { name: "user", path: "/my", Icon: UserRound },
 ];
