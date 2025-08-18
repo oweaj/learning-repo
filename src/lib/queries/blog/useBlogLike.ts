d8b9e1b27085e863903755e1ba7bf700f91e41bd
oweaj <qwe1520156@naver.com>
2025-08-18 23:46:31 +0900

    feat: 공감한 데이터 등록 및 취소시 캐시 초기화

--- src/lib/queries/blog/useBlogLike.ts
+++ src/lib/queries/blog/useBlogLike.ts
@@ -9,6 +9,7 @@ export const useBlogLike = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["blog_list"] });
       queryClient.invalidateQueries({ queryKey: ["blogDetail"] });
+      queryClient.invalidateQueries({ queryKey: ["myLikeBlogs"] });
     },
     onError: (error) => alert(error.message),
   });
