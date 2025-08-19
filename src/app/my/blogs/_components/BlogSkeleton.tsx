9f2801985df904276d5a912f73826a5decca6b75
oweaj <qwe1520156@naver.com>
2025-08-19 22:56:06 +0900

    fix: 스켈레톤 크기 비율 수정

--- src/app/my/blogs/_components/BlogSkeleton.tsx
+++ src/app/my/blogs/_components/BlogSkeleton.tsx
@@ -5,7 +5,7 @@ export const BlogSkeleton = ({ limit }: { limit: number }) => {
     <div className="relative rounded-xl space-y-8">
       {Array.from({ length: limit }).map((_, i) => (
         <div key={`skeleton-${i}`} className="flex gap-10 max-md:gap-4">
-          <div className="relative w-[250px] rounded-xl overflow-hidden aspect-square">
+          <div className="relative w-[250px] rounded-xl overflow-hidden aspect-[4/3]">
             <Skeleton className="w-full h-full" />
           </div>
           <div className="w-3/4 flex flex-col justify-between overflow-hidden py-2">
