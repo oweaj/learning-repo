873e90e3515ab7655e83112e2fbab5b29ce28cdd
oweaj <qwe1520156@naver.com>
2025-09-28 23:05:01 +0900

    fix: 유저 정보 api 토큰 유무에 따른 쿼리 실행

--- src/lib/queries/auth/useUser.ts
+++ src/lib/queries/auth/useUser.ts
@@ -1,10 +1,14 @@
 import { getUserApi } from "@/lib/api/auth/auth";
 import type { IUserDataType } from "@/types/auth.type";
 import { useQuery } from "@tanstack/react-query";
+import Cookies from "js-cookie";
 
 export const useUser = () => {
+  const accessToken = Cookies.get("accessToken");
+
   return useQuery<IUserDataType>({
     queryKey: ["user"],
     queryFn: getUserApi,
+    enabled: !!accessToken,
   });
 };
