92ab000ae84aef913b7e1ef126f9bbd5a4e4908d
oweaj <qwe1520156@naver.com>
2025-09-28 23:48:08 +0900

    fix: useUser 재시도 false 설정

--- src/lib/queries/auth/useUser.ts
+++ src/lib/queries/auth/useUser.ts
@@ -1,14 +1,11 @@
 import { getUserApi } from "@/lib/api/auth/auth";
 import type { IUserDataType } from "@/types/auth.type";
 import { useQuery } from "@tanstack/react-query";
-import Cookies from "js-cookie";
 
 export const useUser = () => {
-  const accessToken = Cookies.get("accessToken");
-
   return useQuery<IUserDataType>({
     queryKey: ["user"],
     queryFn: getUserApi,
-    enabled: !!accessToken,
+    retry: 0,
   });
 };
